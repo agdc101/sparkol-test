e7a4da837955e66c034b5649be19806fd55a093c
Dan Tame <dantame@Dans-MBP.lan>
2018-08-10 15:28:28 +0100

    Adds basic authentication flow

--- src/app.js
+++ src/app.js
@@ -1,8 +1,42 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const jwt = require('express-jwt')
+const bodyParser = require('body-parser')
 
-app.get('/', function(req, res) {
-  res.send('hello world')
+const { AUTH_SECRET } = require('./config')
+const { generateToken, verifyUser } = require('./authentication')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use(
+  jwt({
+    secret: AUTH_SECRET,
+  }).unless({ path: ['/login'] })
+)
+
+app.use((err, req, res, next) => {
+  if (err.inner.name === 'TokenExpiredError') {
+    return res.status(401).send('Auth Token Expired')
+  }
+  if (err.name === 'UnauthorizedError') {
+    return res.status(401).send('Invalid Auth Token')
+  }
+  next(req)
+})
+
+app.get('/verifyToken', (req, res) => {
+  res.status(200).send('OK')
+})
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body
+  const user = verifyUser(username, password)
+  if (user) {
+    const token = generateToken(user)
+    return res.status(200).json({ token })
+  }
+  res.status(401).send('Invalid Username or Password')
 })
 
 module.exports = app
