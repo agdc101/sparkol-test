126979002476f86bf564c29208a1c9b34e491c83
agdc101 <agdc101@gmail.com>
2023-06-12 15:33:07 +0100

    started loginform comp

--- sparkol-login-client/src/components/LoginForm.jsx
+++ sparkol-login-client/src/components/LoginForm.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+// src/components/LoginForm.js
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const LoginForm = () => {
+const LoginForm = ({ onLogin }) => {
+  const [userName, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUserLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3333/login', {
+        username : userName,
+        password : password,
+      });
+      onLogin(response.data.token);
+    } catch (error) {
+      console.error('login failed:', error);
+    }
+  };
+
   return (
-    <div>
-      
+    <div class="form-wrapper">
+      <form onSubmit={handleUserLogin}>
+        <input
+          type="text"
+          placeholder="username"
+          value={userName}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Log In</button>
+      </form>
     </div>
   );
-}
+};
 
 export default LoginForm;
